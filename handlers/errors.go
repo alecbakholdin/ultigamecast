c495ef9064ec2f24284b2f9ed3bc9b02cfaa2b12
alecbakholdin <alecbakholdin@gmail.com>
2024-05-22 11:53:59 -0400

    removed unused function

--- handlers/errors.go
+++ handlers/errors.go
@@ -2,15 +2,9 @@ package handlers
 
 import (
 	"errors"
-	"log/slog"
-	"net/http"
 )
 
 var (
 	ErrorInternalServerError = errors.New("unexpected error")
 )
 
-func unexpectedError(w http.ResponseWriter, r *http.Request, err error) {
-	slog.ErrorContext(r.Context(), "Unexpected error", "err", err)
-	http.Error(w, "Unexpected Error", http.StatusInternalServerError)
-}
