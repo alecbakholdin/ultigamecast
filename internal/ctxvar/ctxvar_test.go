548620456dae82f5ae9b6fd2b06671549a4fee2b
alecbakholdin <alecbakholdin@gmail.com>
2024-06-17 20:42:12 -0400

    Fixed ctxvar_test

--- internal/ctxvar/ctxvar_test.go
+++ internal/ctxvar/ctxvar_test.go
@@ -15,7 +15,7 @@ func TestUrlStandardValues(t *testing.T) {
 	ctx = context.WithValue(ctx, Game, &models.Game{Slug: "game-slug", ID: 4})
 	ctx = context.WithValue(ctx, Event, &models.Event{ID: "random-string"})
 
-	assert.Equal(t, "/teams/team-slug/schedule/tournament-slug/players/player-slug/games/game-slug/events/random-string/url", Url(ctx, Team, Tournament, Player, Game, Event, "url"))
+	assert.Equal(t, "/teams/team-slug/schedule/tournament-slug/players/player-slug/schedule/game-slug/events/random-string/url", Url(ctx, Team, Tournament, Player, Game, Event, "url"))
 	assert.Equal(t, "/", Url(ctx, ""))
 	assert.Equal(t, "/teams/team-slug", Url(ctx, "", Team))
 	assert.Panics(t, func() { Url(ctx, "", HttpMethod) })
